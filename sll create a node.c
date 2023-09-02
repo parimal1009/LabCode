2ee8690abe1c2788a9d215f513cdbc754a0ec21b
parimal1009 <118711970+parimal1009@users.noreply.github.com>
2023-09-02 12:16:19 +0530

    Add files via upload

---
+++ sll create a node.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+int main() {
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+
+    head->data = 45;
+    head->next = NULL;
+    
+    printf("%d",head->data);
+
+    free(head);
+
+    return 0;
+}
