724225b31c513b9a7ff1617eabe304c93ce4e824
parimal1009 <118711970+parimal1009@users.noreply.github.com>
2023-09-07 05:59:51 +0530

    Update dll all.c

--- dll all.c
+++ dll all.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure for a doubly linked list node
+// Define a structure for a singly linked list node
 struct node {
     int data;
-    struct node* prev;
     struct node* next;
 };
 
-// Function to create a new node for DLL
+// Function to create a new node for SLL
 struct node* createNode(int data) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
     newNode->data = data;
-    newNode->prev = NULL;
     newNode->next = NULL;
     return newNode;
 }
 
-// Function to append a node to the end of the DLL
+// Function to insert a node at the end of the SLL
 void append(struct node** head, int data) {
     struct node* newNode = createNode(data);
 
@@ -29,25 +27,53 @@ void append(struct node** head, int data) {
             current = current->next;
         }
         current->next = newNode;
-        newNode->prev = current;
     }
 }
 
-// Function to concatenate two DLLs
-void concatenate(struct node** head1, struct node* head2) {
-    if (*head1 == NULL) {
-        *head1 = head2; // If the first list is empty, simply point it to the second list
-    } else if (head2 != NULL) {
-        struct node* current = *head1;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = head2;
-        head2->prev = current;
-    }
+// Function to insert a node at the beginning of the SLL
+void insertAtBeginning(struct node** head, int data) {
+    struct node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
 }
 
-// Function to print the DLL in forward direction
+// Function to delete a node at the end of the SLL
+void deleteAtEnd(struct node** head) {
+    if (*head == NULL) {
+        printf("List is empty, cannot delete.\n");
+        return;
+    }
+
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+
+    struct node* current = *head;
+    struct node* previous = NULL;
+    while (current->next != NULL) {
+        previous = current;
+        current = current->next;
+    }
+
+    free(current);
+    previous->next = NULL;
+}
+
+// Function to delete a node at the beginning of the SLL
+void deleteAtBeginning(struct node** head) {
+    if (*head == NULL) {
+        printf("List is empty, cannot delete.\n");
+        return;
+    }
+
+    struct node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+// Function to print the SLL in forward direction
 void printForward(struct node* head) {
     struct node* current = head;
     while (current != NULL) {
@@ -57,37 +83,73 @@ void printForward(struct node* head) {
     printf("\n");
 }
 
+// Function to reverse the SLL
+void reverse(struct node** head) {
+    struct node* prev = NULL;
+    struct node* current = *head;
+    struct node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+}
+
+// Function to concatenate two SLLs
+void concatenate(struct node** first, struct node* second) {
+    if (*first == NULL) {
+        *first = second;
+    } else {
+        struct node* current = *first;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = second;
+    }
+}
+
 int main() {
-    struct node* list1 = NULL;
-    struct node* list2 = NULL;
+    struct node* head1 = NULL;
+    struct node* head2 = NULL;
 
-    // Append nodes to the first DLL
-    append(&list1, 1);
-    append(&list1, 2);
-    append(&list1, 3);
+    // Append nodes to the end of the first SLL
+    append(&head1, 50);
+    append(&head1, 70);
 
-    // Append nodes to the second DLL
-    append(&list2, 4);
-    append(&list2, 5);
+    // Print the first SLL in forward direction
+    printf("First SLL in forward direction:\n");
+    printForward(head1);
 
-    // Print both DLLs before concatenation
-    printf("DLL 1 before concatenation:\n");
-    printForward(list1);
+    // Append nodes to the end of the second SLL
+    append(&head2, 30);
+    append(&head2, 40);
 
-    printf("DLL 2 before concatenation:\n");
-    printForward(list2);
+    // Print the second SLL in forward direction
+    printf("Second SLL in forward direction:\n");
+    printForward(head2);
 
-    // Concatenate the second DLL to the first DLL
-    concatenate(&list1, list2);
+    // Reverse the first SLL
+    reverse(&head1);
 
-    // Print the concatenated DLL
-    printf("DLL after concatenation:\n");
-    printForward(list1);
+    // Print the first SLL in reverse direction
+    printf("First SLL in reverse direction:\n");
+    printForward(head1);
+
+    // Concatenate the second SLL to the end of the first SLL
+    concatenate(&head1, head2);
+
+    // Print the concatenated SLL
+    printf("Concatenated SLL:\n");
+    printForward(head1);
 
     // Free the memory and clean up
-    while (list1 != NULL) {
-        struct node* temp = list1;
-        list1 = list1->next;
+    while (head1 != NULL) {
+        struct node* temp = head1;
+        head1 = head1->next;
         free(temp);
     }
 
