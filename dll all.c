a980d2b0c0283dee52a9f2ab57292ff96688ef45
parimal1009 <118711970+parimal1009@users.noreply.github.com>
2023-09-11 09:27:04 +0530

    Update dll all.c

--- dll all.c
+++ dll all.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure for a singly linked list node
 struct node {
     int data;
+    struct node* prev;
     struct node* next;
 };
 
-// Function to create a new node for SLL
 struct node* createNode(int data) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
     newNode->data = data;
+    newNode->prev = NULL;
     newNode->next = NULL;
     return newNode;
 }
 
-// Function to insert a node at the end of the SLL
 void append(struct node** head, int data) {
     struct node* newNode = createNode(data);
 
@@ -27,41 +26,42 @@ void append(struct node** head, int data) {
             current = current->next;
         }
         current->next = newNode;
+        newNode->prev = current;
     }
 }
 
-// Function to insert a node at the beginning of the SLL
 void insertAtBeginning(struct node** head, int data) {
     struct node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
+
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
+    }
 }
 
-// Function to delete a node at the end of the SLL
 void deleteAtEnd(struct node** head) {
     if (*head == NULL) {
         printf("List is empty, cannot delete.\n");
         return;
     }
 
-    if ((*head)->next == NULL) {
-        free(*head);
-        *head = NULL;
-        return;
-    }
-
     struct node* current = *head;
-    struct node* previous = NULL;
     while (current->next != NULL) {
-        previous = current;
         current = current->next;
     }
 
-    free(current);
-    previous->next = NULL;
+    if (current->prev == NULL) {
+        free(current);
+        *head = NULL;
+    } else {
+        current->prev->next = NULL;
+        free(current);
+    }
 }
 
-// Function to delete a node at the beginning of the SLL
 void deleteAtBeginning(struct node** head) {
     if (*head == NULL) {
         printf("List is empty, cannot delete.\n");
@@ -70,86 +70,110 @@ void deleteAtBeginning(struct node** head) {
 
     struct node* temp = *head;
     *head = (*head)->next;
+
+    if (*head != NULL) {
+        (*head)->prev = NULL;
+    }
+
     free(temp);
 }
 
-// Function to print the SLL in forward direction
-void printForward(struct node* head) {
-    struct node* current = head;
-    while (current != NULL) {
-        printf("%d ", current->data);
+struct node* concatenate(struct node* list1, struct node* list2) {
+    if (list1 == NULL) {
+        return list2;
+    } else if (list2 == NULL) {
+        return list1;
+    }
+
+    struct node* current = list1;
+    while (current->next != NULL) {
         current = current->next;
     }
-    printf("\n");
+
+    current->next = list2;
+    list2->prev = current;
+
+    return list1;
 }
 
-// Function to reverse the SLL
-void reverse(struct node** head) {
-    struct node* prev = NULL;
-    struct node* current = *head;
-    struct node* next = NULL;
+struct node* unionDLL(struct node* list1, struct node* list2) {
+    struct node* result = NULL;
+    int seen[1000] = {0}; 
 
+    struct node* current = list1;
     while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        if (!seen[current->data]) {
+            append(&result, current->data);
+            seen[current->data] = 1;
+        }
+        current = current->next;
     }
 
-    *head = prev;
+    current = list2;
+    while (current != NULL) {
+        if (!seen[current->data]) {
+            append(&result, current->data);
+            seen[current->data] = 1;
+        }
+        current = current->next;
+    }
+
+    return result;
 }
 
-// Function to concatenate two SLLs
-void concatenate(struct node** first, struct node* second) {
-    if (*first == NULL) {
-        *first = second;
-    } else {
-        struct node* current = *first;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = second;
+struct node* intersection(struct node* list1, struct node* list2) {
+    struct node* result = NULL;
+    int seen[1000] = {0}; 
+
+    struct node* current = list1;
+    while (current != NULL) {
+        seen[current->data]++;
+        current = current->next;
     }
+
+    current = list2;
+    while (current != NULL) {
+        if (seen[current->data] > 0) {
+            append(&result, current->data);
+            seen[current->data] = 0;
+        }
+        current = current->next;
+    }
+
+    return result;
 }
 
 int main() {
     struct node* head1 = NULL;
     struct node* head2 = NULL;
 
-    // Append nodes to the end of the first SLL
     append(&head1, 50);
     append(&head1, 70);
 
-    // Print the first SLL in forward direction
-    printf("First SLL in forward direction:\n");
-    printForward(head1);
-
-    // Append nodes to the end of the second SLL
     append(&head2, 30);
-    append(&head2, 40);
+    append(&head2, 70);
 
-    // Print the second SLL in forward direction
-    printf("Second SLL in forward direction:\n");
-    printForward(head2);
+    head1 = concatenate(head1, head2);
 
-    // Reverse the first SLL
-    reverse(&head1);
+    struct node* unionResult = unionDLL(head1, head2);
 
-    // Print the first SLL in reverse direction
-    printf("First SLL in reverse direction:\n");
-    printForward(head1);
+    struct node* intersectionResult = intersection(head1, head2);
 
-    // Concatenate the second SLL to the end of the first SLL
-    concatenate(&head1, head2);
-
-    // Print the concatenated SLL
-    printf("Concatenated SLL:\n");
-    printForward(head1);
-
-    // Free the memory and clean up
     while (head1 != NULL) {
         struct node* temp = head1;
         head1 = head1->next;
+        free(temp);
+    }
+
+    while (unionResult != NULL) {
+        struct node* temp = unionResult;
+        unionResult = unionResult->next;
+        free(temp);
+    }
+
+    while (intersectionResult != NULL) {
+        struct node* temp = intersectionResult;
+        intersectionResult = intersectionResult->next;
         free(temp);
     }
 
