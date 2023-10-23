a4869eebe32183ed3ca08103bc19d16d8ac4356d
parimal1009 <118711970+parimal1009@users.noreply.github.com>
2023-10-23 09:49:44 +0530

    Update sll all.c

--- DSL/sll all.c
+++ DSL/sll all.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure for a singly linked list node
 struct node {
     int data;
     struct node* next;
 };
 
-// Function to create a new node for SLL
 struct node* createNode(int data) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
     newNode->data = data;
@@ -15,8 +13,13 @@ struct node* createNode(int data) {
     return newNode;
 }
 
-// Function to insert a node at the end of the SLL
-void append(struct node** head, int data) {
+void insertAtBeginning(struct node** head, int data) {
+    struct node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAtLast(struct node** head, int data) {
     struct node* newNode = createNode(data);
 
     if (*head == NULL) {
@@ -30,15 +33,43 @@ void append(struct node** head, int data) {
     }
 }
 
-// Function to insert a node at the beginning of the SLL
-void insertAtBeginning(struct node** head, int data) {
+void insertAtRandomLocation(struct node** head, int data, int position) {
     struct node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
+
+    if (position == 0) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct node* current = *head;
+    int i = 0;
+    while (i < position - 1 && current != NULL) {
+        current = current->next;
+        i++;
+    }
+
+    if (current == NULL) {
+        printf("Invalid position for insertion.\n");
+        free(newNode);
+    } else {
+        newNode->next = current->next;
+        current->next = newNode;
+    }
 }
 
-// Function to delete a node at the end of the SLL
-void deleteAtEnd(struct node** head) {
+void deleteFromBeginning(struct node** head) {
+    if (*head == NULL) {
+        printf("List is empty, cannot delete.\n");
+        return;
+    }
+
+    struct node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+void deleteFromLast(struct node** head) {
     if (*head == NULL) {
         printf("List is empty, cannot delete.\n");
         return;
@@ -61,19 +92,28 @@ void deleteAtEnd(struct node** head) {
     previous->next = NULL;
 }
 
-// Function to delete a node at the beginning of the SLL
-void deleteAtBeginning(struct node** head) {
+void deleteNodeAfterLocation(struct node** head, int position) {
     if (*head == NULL) {
         printf("List is empty, cannot delete.\n");
         return;
     }
 
-    struct node* temp = *head;
-    *head = (*head)->next;
-    free(temp);
+    struct node* current = *head;
+    int i = 0;
+    while (i < position && current != NULL) {
+        current = current->next;
+        i++;
+    }
+
+    if (current == NULL || current->next == NULL) {
+        printf("Invalid position for deletion.\n");
+    } else {
+        struct node* temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
 }
 
-// Function to print the SLL in forward direction
 void printForward(struct node* head) {
     struct node* current = head;
     while (current != NULL) {
@@ -83,7 +123,6 @@ void printForward(struct node* head) {
     printf("\n");
 }
 
-// Function to reverse the SLL
 void reverse(struct node** head) {
     struct node* prev = NULL;
     struct node* current = *head;
@@ -99,7 +138,6 @@ void reverse(struct node** head) {
     *head = prev;
 }
 
-// Function to concatenate two SLLs
 void concatenate(struct node** first, struct node* second) {
     if (*first == NULL) {
         *first = second;
@@ -116,41 +154,62 @@ int main() {
     struct node* head1 = NULL;
     struct node* head2 = NULL;
 
-    // Append nodes to the end of the first SLL
-    append(&head1, 50);
-    append(&head1, 70);
+    int choice;
+    int data;
+    int position;
 
-    // Print the first SLL in forward direction
-    printf("First SLL in forward direction:\n");
-    printForward(head1);
+    while (1) {
+        printf("\n1. Insert at Beginning\n2. Insert at Last\n3. Insert at Random Location\n4. Delete from Beginning\n5. Delete from Last\n6. Delete Node After Specified Location\n7. Reverse\n8. Concatenate\n9. Display\n10. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    // Append nodes to the end of the second SLL
-    append(&head2, 30);
-    append(&head2, 40);
-
-    // Print the second SLL in forward direction
-    printf("Second SLL in forward direction:\n");
-    printForward(head2);
-
-    // Reverse the first SLL
-    reverse(&head1);
-
-    // Print the first SLL in reverse direction
-    printf("First SLL in reverse direction:\n");
-    printForward(head1);
-
-    // Concatenate the second SLL to the end of the first SLL
-    concatenate(&head1, head2);
-
-    // Print the concatenated SLL
-    printf("Concatenated SLL:\n");
-    printForward(head1);
-
-    // Free the memory and clean up
-    while (head1 != NULL) {
-        struct node* temp = head1;
-        head1 = head1->next;
-        free(temp);
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at the beginning: ");
+                scanf("%d", &data);
+                insertAtBeginning(&head1, data);
+                break;
+            case 2:
+                printf("Enter data to insert at the last: ");
+                scanf("%d", &data);
+                insertAtLast(&head1, data);
+                break;
+            case 3:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                printf("Enter the position for insertion: ");
+                scanf("%d", &position);
+                insertAtRandomLocation(&head1, data, position);
+                break;
+            case 4:
+                deleteFromBeginning(&head1);
+                break;
+            case 5:
+                deleteFromLast(&head1);
+                break;
+            case 6:
+                printf("Enter the position to delete after: ");
+                scanf("%d", &position);
+                deleteNodeAfterLocation(&head1, position);
+                break;
+            case 7:
+                reverse(&head1);
+                printf("List reversed.\n");
+                break;
+            case 8:
+                concatenate(&head1, head2);
+                printf("Concatenated.\n");
+                break;
+            case 9:
+                printf("Linked List: ");
+                printForward(head1);
+                break;
+            case 10:
+                printf("Exiting the program.\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
     }
 
     return 0;
